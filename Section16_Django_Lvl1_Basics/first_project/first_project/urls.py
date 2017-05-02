ffa627435265f421ce4bf0dff5f6b84ae5a4f2cc
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2017-05-02 13:26:39 -0300

    Started Django Level 1

--- Section16_Django_Lvl1_Basics/first_project/first_project/urls.py
+++ Section16_Django_Lvl1_Basics/first_project/first_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from first_app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
