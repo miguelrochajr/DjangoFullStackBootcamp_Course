0c3bf8aadbae36c397e1ac91b04103aeaeebc202
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2017-06-05 09:40:10 -0300

    Template inheritance added

--- Section19_Django_Lvl4/learning_templates/basic_app/urls.py
+++ Section19_Django_Lvl4/learning_templates/basic_app/urls.py
@@ -5,7 +5,6 @@ from basic_app import views
 # This basic_app needs to match the one in the URL tagging.
 app_name = 'basic_app'
 
-
 urlpatterns = [
     url(r'^relative/$', views.relative, name='relative'),
     url(r'^other/$', views.other, name='other'),
